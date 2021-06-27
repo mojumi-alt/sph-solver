68f82e2be3572705c2241c47b2cf86842876e585
Moritz Milla <milla.moritz@gmail.com>
2021-06-27 18:44:37 +0200

    Remove unused integrator code.

--- src/Integrator.cc
+++ src/Integrator.cc
@@ -12,9 +12,7 @@ void integrate(ParticleSystem &ps, const float dt)
         pi.s += pi.v * dt;
     }
 
-    ps.compute_acceleration();
-    for (auto &pi : ps.get_particles())
-        pi.v += pi.a * (dt / 2.f);
+
 }
 
 } // namespace sph
