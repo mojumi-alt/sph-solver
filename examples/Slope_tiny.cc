8725cde0db9c09400e8c334cb384ac8b1ea288d5
Moritz Milla <milla.moritz@gmail.com>
2021-07-10 16:11:28 +0200

    Added more examples.

---
+++ examples/Slope_tiny.cc
@@ -0,0 +1,23 @@
+#include "Obstacle.hpp"
+#include "Particle.hpp"
+#include "ParticleSystem.hpp"
+#include "Visualizer.hpp"
+
+int main()
+{
+    std::vector<sph::Particle> particles;
+    std::vector<sph::Obstacle> obstacles {
+        sph::Obstacle({0, 5}, {10, 10}),
+    };
+    
+    float width = 10, height = 10, size = 0.15;
+    
+    for (float x = size; x < 4; x += size - 0.01)
+        for (float y = size; y < 5 - size; y += size - 0.01)
+            particles.push_back(sph::Particle{.s = {x, y}});
+
+    sph::ParticleSystem ps(particles, {width, height}, obstacles, {}, size);
+
+    sph::Visualizer w(ps, 80);
+    w.run();
+}
---
+++ examples/Steady_tiny.cc
@@ -0,0 +1,19 @@
+#include "Particle.hpp"
+#include "ParticleSystem.hpp"
+#include "Visualizer.hpp"
+
+int main()
+{
+    std::vector<sph::Particle> particles;
+    
+    float width = 10, height = 10, size = 0.15;
+    
+    for (float x = 3; x < 7; x += size - 0.01)
+        for (float y = 6; y < 10 - size; y += size - 0.01)
+            particles.push_back(sph::Particle{.s = {x, y}});
+
+    sph::ParticleSystem ps(particles, {width, height}, {}, {}, size);
+
+    sph::Visualizer w(ps, 80);
+    w.run();
+}
