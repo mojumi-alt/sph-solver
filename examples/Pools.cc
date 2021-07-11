22df25d19c3a00f04cfe06d50c9a9a20062e9f9f
Moritz Milla <milla.moritz@gmail.com>
2021-07-11 13:29:30 +0200

    Add new fun example.

---
+++ examples/Pools.cc
@@ -0,0 +1,32 @@
+#include "Inflow.hpp"
+#include "Obstacle.hpp"
+#include "Particle.hpp"
+#include "ParticleSystem.hpp"
+#include "Visualizer.hpp"
+
+int main()
+{
+    float width = 10, height = 10, size = 0.2;
+
+    std::vector<sph::Obstacle> obstacles{
+        sph::Obstacle({0.5, 0.5}, {5.0, 0.5}),
+        sph::Obstacle({0.5, 0.5}, {0.5, 3.5}),
+        sph::Obstacle({0.5, 3.5}, {5.0, 3.5}),
+        sph::Obstacle({5.0, 3.5}, {5.0, 2.5}),
+        sph::Obstacle({5.0, 0.5}, {5.0, 1.0}),
+
+        sph::Obstacle({4.0, 7.0}, {10.0, 7.0}),
+        sph::Obstacle({4.0, 7.0}, {4.0, 5.5}),
+        sph::Obstacle({4.0, 3.5}, {4.0, 4.0}),
+    };
+
+    std::vector<sph::Inflow> inflows{
+        sph::Inflow({1.0, 1.0, 2, 2}, 0.2, {0.0, 0.0})};
+
+    inflows[0].end_time = 200;
+
+    sph::ParticleSystem ps({}, {width, height}, obstacles, inflows, size);
+
+    sph::Visualizer w(ps, 80);
+    w.run();
+}
--- examples/Simple.cc
+++ examples/Simple.cc
@@ -1,60 +1,29 @@
-
-#include <iostream>
-#include <random>
-
 #include "Inflow.hpp"
 #include "Obstacle.hpp"
 #include "Particle.hpp"
 #include "ParticleSystem.hpp"
 #include "Visualizer.hpp"
 
-// int main()
-// {
-//     std::vector<sph::Particle> particles;
-//     std::mt19937 mt;
-//     std::uniform_real_distribution<float> dist(-1.0, 1.0);
-
-//     float width = 10, height = 10, size = 0.3;
-
-//     std::vector<sph::Obstacle> obstacles{
-//     //    sph::Obstacle({0, 5}, {10, 5}),
-//     };
-
-//     for (float x = 3; x < 7; x += size - 0.01)
-//         for (float y = 6; y < 10 - size; y += size - 0.01)
-//             particles.push_back(sph::Particle{.s = {x, y}});
-
-//     //for (float x = 3; x < 7; x += size - 0.01)
-//     //    for (float y = 1; y < 5 - size; y += size - 0.01)
-//     //        particles.push_back(sph::Particle{.s = {x, y}});
-
-//     sph::ParticleSystem ps(particles, {width, height}, obstacles, size);
-
-//     sph::Visualizer w(ps, 80);
-//     w.run();
-//     return 0;
-// }
-
 int main()
 {
-    float width = 10, height = 10, size = 0.25;
+    float width = 10, height = 10, size = 0.2;
 
-    std::vector<sph::Obstacle> obstacles{};
+    std::vector<sph::Obstacle> obstacles{
+        sph::Obstacle({0.5, 0.5}, {5.0, 0.5}),
+        sph::Obstacle({0.5, 0.5}, {0.5, 3.5}),
+        sph::Obstacle({0.5, 3.5}, {5.0, 3.5}),
+        sph::Obstacle({5.0, 3.5}, {5.0, 2.5}),
+        sph::Obstacle({5.0, 0.5}, {5.0, 1.0}),
 
-    float interval = 0.2;
-    for (float f = 0.0; f < 10.0; f += interval)
-    {
-        auto funct = [](double pos_x) -> float {
-            return 0.2 * (pos_x - 5) * (pos_x - 5);
-        };
-        obstacles.push_back(sph::Obstacle(
-            {f, 10 - funct(f)}, {f + interval, 10 - funct(f + interval)}));
-    }
+        sph::Obstacle({4.0, 7.0}, {10.0, 7.0}),
+        sph::Obstacle({4.0, 7.0}, {4.0, 5.5}),
+        sph::Obstacle({4.0, 3.5}, {4.0, 4.0}),
+    };
 
     std::vector<sph::Inflow> inflows{
-        sph::Inflow({4.0, 1.0, 2, 1}, 0.1, {0.0, 1.0})};
+        sph::Inflow({1.0, 1.0, 2, 2}, 0.2, {0.0, 0.0})};
 
-    inflows[0].end_time = 10;
+    inflows[0].end_time = 200;
 
     sph::ParticleSystem ps({}, {width, height}, obstacles, inflows, size);
 
